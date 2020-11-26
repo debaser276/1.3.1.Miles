946e1a8fe124e634715f8c0954677380cc024711
debaser <debaser86@yandex.ru>
2020-11-26 22:24:19 +0300

    BonusMilesService added

---
+++ src/BonusMilesService.java
@@ -0,0 +1,6 @@
+public class BonusMilesService {
+    public int calculate(int cost) {
+        int divider = 20;
+        return cost / divider;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int ticketPrice = 2340;
-        int divider = 20;
-        int miles = ticketPrice / divider;
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
         System.out.println(miles);
     }
 }
