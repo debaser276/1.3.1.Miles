80dee21b20e25cc2866fede795a6e57d6e3c7e02
debaser <debaser86@yandex.ru>
2020-11-26 11:28:36 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 2340;
+        int divider = 20;
+        int miles = ticketPrice / divider;
+        System.out.println(miles);
+    }
+}
